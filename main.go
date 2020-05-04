dc1f48fc72d6a0fb0514e14c645b7be53e9241b9
Ankit Bahuguna <ankiit.bahuguna@gmail.com>
2020-05-04 20:01:06 +0530

    Clean main package:

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ import (
 
 	"github.com/ankibahuguna/news/parser"
 	"github.com/ankibahuguna/news/request"
-	"github.com/ankibahuguna/news/types"
 	"github.com/ankibahuguna/news/ui"
-	"github.com/fatih/color"
 )
 
 func main() {
@@ -31,9 +29,8 @@ func main() {
 		panic("Something went wrong")
 	}
 
-	headlines := getHeadLines(news)
 	for {
-		index, err := ui.ShowHeadLines("HeadLines", 20, headlines)
+		index, err := ui.ShowHeadLines("HeadLines", 20, news)
 
 		if err != nil {
 			fmt.Printf("Prompt failed %v\n", err)
@@ -56,28 +53,4 @@ func main() {
 		ui.ShowPrompt()
 	}
 
-}
-
-func getHeadLines(news []types.News) []string {
-	var headlines []string
-	green := color.New(color.FgGreen).SprintFunc()
-	white := color.New(color.FgWhite).SprintFunc()
-
-	for _, val := range news {
-		title, description := val.Title, val.Description
-		totalLength := len(title+description) - len(description)
-
-		description = description[0:min(len(description), totalLength)] + "..."
-		titleString := fmt.Sprintf("%s (%s)", white(title), green(description))
-		headlines = append(headlines, titleString)
-	}
-
-	return headlines
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
 }
--- ui/list.go
+++ ui/list.go
@@ -3,10 +3,14 @@ package ui
 import (
 	"fmt"
 
+	"github.com/ankibahuguna/news/types"
+	"github.com/fatih/color"
 	"github.com/manifoldco/promptui"
 )
 
-func ShowHeadLines(label string, size int, items []string) (int, error) {
+func ShowHeadLines(label string, size int, news []types.News) (int, error) {
+
+	items := getHeadLines(news)
 
 	prompt := promptui.Select{
 		Label: label,
@@ -22,4 +26,28 @@ func ShowHeadLines(label string, size int, items []string) (int, error) {
 	}
 
 	return index, nil
+}
+
+func getHeadLines(news []types.News) []string {
+	var headlines []string
+	green := color.New(color.FgGreen).SprintFunc()
+	white := color.New(color.FgWhite).SprintFunc()
+
+	for _, val := range news {
+		title, description := val.Title, val.Description
+		totalLength := len(title+description) - len(description)
+
+		description = description[0:min(len(description), totalLength)] + "..."
+		titleString := fmt.Sprintf("%s (%s)", white(title), green(description))
+		headlines = append(headlines, titleString)
+	}
+
+	return headlines
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
